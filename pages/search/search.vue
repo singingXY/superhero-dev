518c5f0509197186ce26a5e9b04310ec320497be
singingXY <singeve@qq.com>
2020-01-06 15:28:38 +0800

    触底刷新

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,6 +1,22 @@
 <template>
 	<view class="page">
-		搜索页
+		<view class="search-block">
+			<view class="search-ico-wapper">
+				<image src="/static/icons/search.png" class="search-ico"></image>
+			</view>
+			<input type="text" value=""
+				placeholder="搜索影片"
+				maxlength="10"
+				class="search-text"
+				confirm-type="search"
+				@confirm="searchMe"
+			/>
+		</view>
+		<view class="movie-list page-block">
+			<view class="movie-wapper" v-for="trailer in trailerList" :key="trailer.id">
+				<image :src="trailer.cover" class="poster"></image>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +24,90 @@
 	export default {
 		data() {
 			return {
-				
+				trailerList: [],
+				keywords: [],  // 搜索关键字
+				page: 1,       // 当前页数
+				totalPages: 1  // 总页数
 			}
 		},
-		methods: {
+		onLoad() {
+			var _this = this;
 			
+			uni.showLoading({
+				mask:true,
+				title:"请稍后…"
+			})
+			uni.showNavigationBarLoading()
+			
+			//查询猜你喜欢
+			uni.request({
+				url: _this.serverURL + '/search/list?keyword=&page=&pageSize', 
+				method:"POST",
+				success: (res) => {
+					if(res.data.status == 200){
+						_this.trailerList = res.data.data.rows;
+					}
+				},
+				complete: () => {
+					uni.hideLoading()
+					uni.hideNavigationBarLoading()
+				}
+			});
+		},
+		onReachBottom() {
+			var _this = this;
+			var page = _this.page + 1 
+			var keywords = _this.keywords
+			var totalPages = _this.totalPages
+			if( page > totalPages){ return;}
+			
+			this.pagedTrailerList(keywords,page,15)
+		},
+		methods: {
+			pagedTrailerList(keywords,page,pageSize){
+				var _this = this;
+				
+				uni.showLoading({
+					mask:true,
+					title:"请稍后…"
+				})
+				uni.showNavigationBarLoading()
+				
+				//查询猜你喜欢
+				uni.request({
+					url: _this.serverURL
+						+ '/search/list?keywords=' + keywords
+						+ '&page=' + page
+						+ '&pageSize=' + pageSize, 
+					method:"POST",
+					success: (res) => {
+						if(res.data.status == 200){
+							var tempList = res.data.data.rows;
+						console.log(_this.trailerList);
+							_this.trailerList = _this.trailerList.concat(tempList)
+						console.log(_this.trailerList);
+							_this.totalPages =res.data.data.total //总页数
+							_this.page = page
+						}
+					},
+					complete: () => {
+						uni.hideLoading()
+						uni.hideNavigationBarLoading()
+					}
+				});
+			},
+			searchMe(e){
+				var _this = this
+				//获取搜索内容
+				var value = e.detail.value
+				this.keywords = value
+				this.trailerList = []
+				this.pagedTrailerList(value,1,15)
+			}
 		}
 	}
 </script>
 
 <style>
-
+@import url("search.css");
 </style>
