59cfd9d419dd03ae3ad1d52651464b55fabd708a
singingXY <singeve@qq.com>
2020-01-10 14:23:46 +0800

    Create aline.vue

---
+++ components/aline.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="line-wappar">
+		<view class="line"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"aline"
+	}
+</script>
+
+<style>
+	.line-wappar{padding: 0 20upx;}
+	.line{height: 1px;background-color: #dbdbda}
+</style>
