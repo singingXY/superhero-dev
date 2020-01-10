86e5b64b8e6df8dbd04574a5f04d780df153e9f4
singingXY <singeve@qq.com>
2020-01-10 14:25:07 +0800

    详情页--演职人员、剧照

--- pages/movie/movie.vue
+++ pages/movie/movie.vue
@@ -19,7 +19,7 @@
 				
 				<view class="score-block">
 					<view class="big-score">
-						<view class="score-word">综合评分:</view>
+						<view class="score-word">综合评分：</view>
 						<view class="movie-score">{{trailerInfo.score}}</view>
 					</view>
 					<view class="score-stars">
@@ -36,6 +36,7 @@
 			</view>
 		</view>
 		<!-- 影片基本信息  end-->
+		<aline></aline>
 		<view class="plots-block">
 			<view class="plots-title">剧情介绍</view>
 			<view class="plots-desc">
@@ -43,28 +44,88 @@
 			</view>
 		</view>
 		<!-- 剧情介绍  end-->
+		<view class="scroll-block">
+			<view class="plots-title">演职人员</view>
+			<scroll-view scroll-x="true" class="scroll-list">
+				<view class="actor-wapper"
+					v-for="director in directorArray"
+					:key="director.staffId">
+					<image
+					 :src="director.photo" class="single-actor"
+					 mode="aspectFill"></image>
+					 <view class="actor-name">{{director.name}}</view>
+					 <view class="actor-role">{{director.actName}}</view>
+				</view>
+				<view class="actor-wapper"
+					 v-for="actor in actorArray"
+					 :key="actor.staffId">
+					<image
+					 :src="actor.photo" class="single-actor"
+					 mode="aspectFill"></image>
+					 <view class="actor-name">{{actor.name}}</view>
+					 <view class="actor-role">{{actor.actName}}</view>
+				</view>
+			</scroll-view>
+		</view>
+		<!-- 演职人员 end -->
+		<view class="scroll-block">
+			<view class="plots-title">剧照</view>
+			<scroll-view scroll-x="true" class="scroll-list">
+				<image
+				 v-for="(img,index) in plotPicsArray"
+				 :key="index"
+				 :src="img" class="plot-images"
+				 mode="aspectFill"></image>
+			</scroll-view>
+		</view>
+		<!-- 剧照 end -->
 		
 	</view>
 </template>
 
 <script>
 	import trailerStars from "../../components/trailerStars.vue"
+	import aline from "../../components/aline.vue"
 	export default {
 		data() {
 			return {
-				trailerInfo:{}
+				trailerInfo:{},   //详情
+				plotPicsArray:[], //剧照列表
+				directorArray:[], //导演列表
+				actorArray:[]     //演员列表
 			}
 		},
 		onLoad(params) {
 			var _this = this;
 			var trailerId = params.trailerId
+			//请求影片详情
 			uni.request({
-				url: _this.serverURL + '/search/trailer?trailerId='+ trailerId, 
+				url: _this.serverURL + '/search/trailer/'+ trailerId, 
 				method:"POST",
 				success: (res) => {
 					if(res.data.status == 200){
 						_this.trailerInfo = res.data.data[0];
-					console.log(_this.trailerInfo);
+						_this.plotPicsArray = _this.trailerInfo.plotPics;
+					}
+				}
+			});
+			//获取导演信息
+			uni.request({
+				url: _this.serverURL + '/search/staff/'+ trailerId + '/1' , 
+				method:"POST",
+				success: (res) => {
+					if(res.data.status == 200){
+						_this.directorArray = res.data.data;
+					}
+				}
+			});
+			//获取演员信息
+			uni.request({
+				url: _this.serverURL + '/search/staff/'+ trailerId + '/2' , 
+				method:"POST",
+				success: (res) => {
+					if(res.data.status == 200){
+						_this.actorArray = res.data.data;
 					}
 				}
 			});
@@ -73,7 +134,8 @@
 			
 		},
 		components:{
-			trailerStars
+			trailerStars,
+			aline
 		}
 	}
 </script>
