7a1934823fa9ecdbd1829319635b110146495b2f
singingXY <singeve@qq.com>
2020-01-11 09:28:38 +0800

    cover

---
+++ pages/cover/cover.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="black">
+		<image src="../../static/icons/guess-u-like.png"
+		class="cover"
+		mode="widthFix"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			uni.setNavigationBarColor({
+				frontColor:"#ffffff",
+				backgroundColor:"#000000"
+			})
+			
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.black{
+	background-color: #000000;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	position: fixed;
+}
+.cover{align-self: center;}
+</style>
