c9e4974f931577397829512e8d1d3d19ca84f8ea
singingXY <singeve@qq.com>
2020-01-17 15:08:05 +0800

    ghp

--- common/common.js
+++
@@ -1,8 +0,0 @@
-// 生产环境
-// const serverURL = "https://www.fastmock.site/mock/4f1424515a291bb34874c2b49a32f141/superhero"
-// 开发环境
-  const serverURL = "https://www.fastmock.site/mock/4f1424515a291bb34874c2b49a32f141/superhero"
-  
-export default{
-	serverURL
-}
